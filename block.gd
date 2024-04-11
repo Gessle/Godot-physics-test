95960de99c107d09d6a8b7f84ad44a5e7d0b475d
Erno <gessle@gpiste.org>
2024-04-11 20:37:01 +0300

    v0.0.1

---
+++ block.gd
@@ -0,0 +1,22 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("1x1")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_input_event(_viewport, _event, _shape_idx):
+	if Input.is_action_pressed("CLICK"):
+		print("clicked")
+		queue_free()
+
+
+func _on_mouse_entered():
+	pass
+
